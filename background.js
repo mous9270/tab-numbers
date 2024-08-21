c723d99ac58d091c16213aaf0c76972eebcb354d
mous9270 <mous9270222@gmail.com>
2024-08-21 04:00:06 -0400

    Display tab number of the current tab in the current web page. Partially useful but not good enough.

--- background.js
+++ background.js
@@ -1,8 +1,19 @@
-chrome.tabs.onActivated.addListener(() => {
-  chrome.tabs.query({}, (tabs) => {
+function updateTabNumbers() {
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
     tabs.forEach((tab, index) => {
-      chrome.tabs.sendMessage(tab.id, { tabNumber: index + 1 });
+      chrome.tabs.sendMessage(tab.id, {action: "updateNumber", number: index + 1})
+        .catch(error => {
+          console.log(`Couldn't send message to tab ${tab.id}. This is normal for new tabs or special Chrome pages.`);
+        });
     });
   });
-});
+}
 
+// Update numbers when tabs change
+chrome.tabs.onActivated.addListener(updateTabNumbers);
+chrome.tabs.onCreated.addListener(updateTabNumbers);
+chrome.tabs.onRemoved.addListener(updateTabNumbers);
+chrome.tabs.onMoved.addListener(updateTabNumbers);
+
+// Initial update
+updateTabNumbers();
